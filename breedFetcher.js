3ecf0fcad717c331d8fdef316bff17f4717096be
robinwebber <robinewebber@gmail.com>
2019-07-04 19:38:07 +0000

    refactored breedFetcher to run from index.js, added mocha and chai testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,24 @@
 const request = require('request');
-const args = process.argv.slice(2);
+//const args = process.argv.slice(2);
 
+const fetchBreedDescription = function(breedname, callback) {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedname}`, (error, response, body) => {
+    
+    const data = JSON.parse(body);
+    if (data.status === 404) {
+      callback(`fur-oh-fur page not found...`, null);
+      //return;
+    } else if (body === "[]") {
+      callback(`Error ❌ ${breedname} does not exist in our database`, null);
+      //return;
+    } else {
+      callback(null, data[0].description);
+    }
+    
+  });
   
-  const data = JSON.parse(body);
-  if (data.status === 404) {
-    console.log(`fur-oh-fur page not found...`);
-    return;
-  }
-  
-  if (body === "[]") {
-    console.log(`Error ❌ ${args[0]} does not exist in our database`);
-    return;
-  }
-  
-  console.log(data[0].description);
-  
-});
+};
+
+module.exports = { fetchBreedDescription };
 
